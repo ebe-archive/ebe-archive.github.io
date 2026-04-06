12771a440262dcd5599d37ee265994efe855cd85
@royashbrook <royashbrook@me.com>
2026-04-06 11:54:58 -0400

    switch from snowpack to vite

---
+++ vite.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills(),
+    svelte(),
+  ],
+})
